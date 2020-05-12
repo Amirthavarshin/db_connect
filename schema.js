c95f0a7fa2d3aab50734ff708ec2fa2b0125ac00
Amirthavarshini <47815743+Amirthavarshin@users.noreply.github.com>
2020-05-13 00:27:21 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,14 +1,14 @@
 let mongoose  = require("./mongoconfig");
 mongoose = mongoose.mongoose
 
-const MenuSchema = new mongoose.Schema({ ProductName: String, ProductId:Number, Description: String, Units:Number, Price:Number});
-const Menu = mongoose.model('tvs', MenuSchema);
+const MenuSchema = new mongoose.Schema({ProductId : Number, ProductName : String, Units : Number, Description : String, Price : Number});
+const tvs = mongoose.model('tvs', MenuSchema);
 
 
 
 // const orderSchema = new mongoose.Schema({ table:Number, order:Array});
-// const Order = mongoose.model('orders', orderSchema);
+// const tvs = mongoose.model('orders', orderSchema);
 
 
- module.exports.Menu = Menu;
-// module.exports.Order= Order;
+ //module.exports.Menu = Menu;
+ module.exports.tvs = tvs;
