af411e37a52dc4b94d06ea06801e96fd6812f807
Amirthavarshini <47815743+Amirthavarshin@users.noreply.github.com>
2020-05-13 00:26:27 +0530

    Update mongoconfig.js

--- mongoconfig.js
+++ mongoconfig.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0-5eye0.mongodb.net/test?retryWrites=true&w=majority').then(()=>{
+//mongodb+srv://username:pass@clusterurl/database
+mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0-5eye0.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+   // useUnifiedTopology: true
+}).then(()=>{
     console.log("Connected sucessfully...");
 
 }).catch((err)=>{
