9c14fcbd1bcbcc58ba84814378b32045b91b7515
Amirthavarshini <47815743+Amirthavarshin@users.noreply.github.com>
2020-05-14 11:30:49 +0530

    Update mongoconfig.js

--- mongoconfig.js
+++ mongoconfig.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 //mongodb+srv://username:pass@clusterurl/database
-mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0-5eye0.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0-5eye0.mongodb.net/tvs?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useCreateIndex: true,
+    dbName: "SpareParts"
    // useUnifiedTopology: true
 }).then(()=>{
     console.log("Connected sucessfully...");
